66d916a3de6e22df7fbb9fa6e9234d7941765911
KareemWebber <kareemkhedr6@gmail.com>
2023-02-26 10:15:25 +0200

    1

--- Dog.js
+++ Dog.js
@@ -9,7 +9,7 @@ class Dog {
         const{name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this
         
         return `
-        <img id="like-stamp" src="public/images/badge-like.png" alt="Like stamp">
+        <img id="like-stamp" src="public/assets/images/badge-like.png" alt="Like stamp">
         <img id="dislike-stamp" src="public/assets/images/badge-nope.png" alt="dislike stamp">
         <img id="tin-dog" src=${avatar} alt="photo of the dog to be liked or disliked">
         <h2 id="dog-name">${name}, ${age}</h2>
