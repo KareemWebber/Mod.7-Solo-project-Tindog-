04ce7a915a0714db0b2e565bb248e2ac28c59f56
KareemWebber <kareemkhedr6@gmail.com>
2023-02-26 09:58:04 +0200

    Fixing images

--- Dog.js
+++ Dog.js
@@ -9,8 +9,8 @@ class Dog {
         const{name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this
         
         return `
-        <img id="like-stamp" src="images/badge-like.png" alt="Like stamp">
-        <img id="dislike-stamp" src="images/badge-nope.png" alt="dislike stamp">
+        <img id="like-stamp" src="public/images/badge-like.png" alt="Like stamp">
+        <img id="dislike-stamp" src="public/images/badge-nope.png" alt="dislike stamp">
         <img id="tin-dog" src=${avatar} alt="photo of the dog to be liked or disliked">
         <h2 id="dog-name">${name}, ${age}</h2>
         <h5 id="phrase">${bio}</h4>
--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 const dogs = [
     {
         name: "Rex",
-        avatar: "images/dog-rex.jpg",
+        avatar: "public/images/dog-rex.jpg",
         age: 25,
         bio: "Art. Literature. Natural wine. Yoga.",
         hasBeenSwiped: false,
         hasBeenLiked: false
     },{
         name: "Bella",
-        avatar: "images/dog-bella.jpg",
+        avatar: "public/images/dog-bella.jpg",
         age: 43,
         bio: "Yup, that's my owner. U can meet him if you want",
         hasBeenSwiped: false,
@@ -16,7 +16,7 @@ const dogs = [
     },
     {
         name: "Teddy",
-        avatar: "images/dog-teddy.jpg",
+        avatar: "public/images/dog-teddy.jpg",
         age: 30,
         bio: "How you doin?",
         hasBeenSwiped: false,
